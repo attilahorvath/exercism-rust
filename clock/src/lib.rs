c37dcfb1d24eeac8e1247fbc5eb3bd9129f3d398
Attila Horváth <hun.ati500@gmail.com>
2017-09-28 00:10:45 +0100

    Simplify solution for Clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,40 +1,28 @@
 use std::fmt;
 
+const MINUTES_IN_A_DAY: i32 = 24 * 60;
+
 #[derive(Debug, PartialEq)]
-pub struct Clock {
-    hours: i32,
-    minutes: i32
-}
+pub struct Clock(i32);
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Clock {
-        let mut hours_offset = minutes / 60;
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        let mut minutes = (hours * 60 + minutes) % MINUTES_IN_A_DAY;
 
         if minutes < 0 {
-            hours_offset -= 1;
+            minutes += MINUTES_IN_A_DAY;
         }
 
-        let mut hours = (hours + hours_offset) % 24;
-        let mut minutes = minutes % 60;
-
-        if hours < 0 {
-            hours += 24;
-        }
-
-        if minutes < 0 {
-            minutes += 60;
-        }
-
-        Clock { hours: hours, minutes: minutes }
+        Clock(minutes)
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Clock {
-        Clock::new(self.hours, self.minutes + minutes)
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        Clock::new(0, self.0 + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(f, "{:02}:{:02}", self.0 / 60, self.0 % 60)
     }
 }
