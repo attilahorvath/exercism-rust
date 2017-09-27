468934c889b6776b1d5c599f91489e8e189f6a3a
Attila Horváth <hun.ati500@gmail.com>
2017-09-27 22:16:46 +0100

    Simplify solution for Circular Buffer

--- circular-buffer/src/lib.rs
+++ circular-buffer/src/lib.rs
@@ -1,7 +1,4 @@
-pub struct CircularBuffer<T> {
-    buffer: Vec<T>,
-    capacity: usize
-}
+pub struct CircularBuffer<T>(Vec<T>);
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -11,37 +8,34 @@ pub enum Error {
 
 impl<T> CircularBuffer<T> {
     pub fn new(capacity: usize) -> Self {
-        CircularBuffer {
-            buffer: Vec::with_capacity(capacity),
-            capacity: capacity
-        }
+        CircularBuffer(Vec::with_capacity(capacity))
     }
 
     pub fn write(&mut self, item: T) -> Result<(), Error> {
-        if self.buffer.len() == self.capacity {
+        if self.0.len() == self.0.capacity() {
             return Err(Error::FullBuffer)
         }
 
-        Ok(self.buffer.push(item))
+        Ok(self.0.push(item))
     }
 
     pub fn overwrite(&mut self, item: T) {
-        if self.buffer.len() == self.capacity {
-            self.buffer.remove(0);
+        if self.0.len() == self.0.capacity() {
+            self.0.remove(0);
         }
 
-        self.buffer.push(item);
+        self.0.push(item);
     }
 
     pub fn read(&mut self) -> Result<T, Error> {
-        if self.buffer.len() == 0 {
+        if self.0.len() == 0 {
             return Err(Error::EmptyBuffer)
         }
 
-        Ok(self.buffer.remove(0))
+        Ok(self.0.remove(0))
     }
 
     pub fn clear(&mut self) {
-        self.buffer.clear();
+        self.0.clear();
     }
 }
