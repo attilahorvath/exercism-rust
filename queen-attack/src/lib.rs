b15b14d6ba26999a547e0abcd7f432b4a37dd6d4
Attila Horváth <hun.ati500@gmail.com>
2017-10-04 00:52:03 +0100

    Add solution for Queen Attack

---
+++ queen-attack/src/lib.rs
@@ -0,0 +1,31 @@
+pub struct ChessPosition {
+    row: i32,
+    column: i32
+}
+
+impl ChessPosition {
+    pub fn new(row: i32, column: i32) -> Result<Self, &'static str> {
+        if row < 0 || row > 7 || column < 0 || column > 7 {
+            return Err("Invalid position on the chess board")
+        }
+
+        Ok(ChessPosition { row: row, column: column })
+    }
+}
+
+pub struct Queen {
+    position: ChessPosition
+}
+
+impl Queen {
+    pub fn new(position: ChessPosition) -> Self {
+        Queen { position: position }
+    }
+
+    pub fn can_attack(&self, other: &Self) -> bool {
+        self.position.row == other.position.row ||
+        self.position.column == other.position.column ||
+        (self.position.row - other.position.row).abs() ==
+        (self.position.column - other.position.column).abs()
+    }
+}
