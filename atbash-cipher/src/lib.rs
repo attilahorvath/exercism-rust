4f0c46d86ea2369fc2716f76e4c7dd70a3cb3972
Attila Horváth <ahorvath@fusioneer.com>
2017-10-09 16:24:40 +0100

    Add solution for Atbash Cipher

---
+++ atbash-cipher/src/lib.rs
@@ -0,0 +1,28 @@
+fn encode_char(c: char) -> char {
+    if c.is_alphabetic() {
+        return ('a' as u8 + ('z' as u8 - c as u8)) as char;
+    }
+
+    c
+}
+
+pub fn encode(text: &str) -> String {
+    text.to_lowercase()
+        .chars()
+        .filter(|&c| {
+            (c as u8 >= 'a' as u8 && c as u8 <= 'z' as u8) || c.is_numeric()
+        })
+        .map(encode_char)
+        .collect::<Vec<_>>()
+        .chunks(5)
+        .map(|c| c.iter().collect::<String>())
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+pub fn decode(text: &str) -> String {
+    text.chars()
+        .filter(|c| c.is_alphanumeric())
+        .map(encode_char)
+        .collect()
+}
