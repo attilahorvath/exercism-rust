61894229fa85021f776f0e1ca395ff06f5b1f834
Attila Horváth <hun.ati500@gmail.com>
2017-10-11 00:09:23 +0100

    Add solution for Luhn

---
+++ luhn/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn is_valid(number: &str) -> bool {
+    let filtered = number.chars().filter(|&c| c != ' ').collect::<String>();
+
+    if filtered.len() <= 1 || filtered.chars().any(|c| !c.is_numeric()) {
+        return false;
+    }
+
+    filtered
+        .chars()
+        .rev()
+        .enumerate()
+        .map(|(index, c)| {
+            let digit = c.to_digit(10).unwrap();
+
+            if index % 2 == 0 {
+                digit
+            } else {
+                let doubled = digit * 2;
+
+                doubled - (if doubled > 9 { 9 } else { 0 })
+            }
+        })
+        .sum::<u32>() % 10 == 0
+}
