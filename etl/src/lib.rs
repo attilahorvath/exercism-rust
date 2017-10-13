b9a2f16580ba611b666966e81a0cb02a62e3a1a2
Attila Horváth <ahorvath@fusioneer.com>
2017-10-13 13:49:47 +0100

    Use iterators for Etl

--- etl/src/lib.rs
+++ etl/src/lib.rs
@@ -1,13 +1,12 @@
 use std::collections::BTreeMap;
 
 pub fn transform(input: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut output = BTreeMap::new();
-
-    for (&score, chars) in input {
-        for char in chars.iter().collect::<String>().to_lowercase().chars() {
-            output.insert(char, score);
-        }
-    }
-
-    output
+    input
+        .iter()
+        .flat_map(|(&score, chars)| {
+            chars.iter().flat_map(|&c| c.to_lowercase()).map(move |c| {
+                (c, score)
+            })
+        })
+        .collect()
 }
