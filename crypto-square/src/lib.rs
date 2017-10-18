500adddb73b40aae388d5aa417bd87c366fe469a
Attila Horváth <hun.ati500@gmail.com>
2017-10-18 02:02:49 +0100

    Improve solution for Crypto Square

--- crypto-square/src/lib.rs
+++ crypto-square/src/lib.rs
@@ -4,28 +4,14 @@ pub fn encrypt(text: &str) -> String {
         .filter(|c| c.is_lowercase())
         .collect::<Vec<_>>();
 
-    if text.is_empty() {
-        return String::new();
-    }
+    let size = (text.len() as f64).sqrt().ceil() as usize;
 
-    let rows = (text.len() as f64).sqrt() as usize;
-    let cols = (text.len() as f64 / rows as f64).ceil() as usize;
-
-    let mut encrypted = String::with_capacity(rows * cols);
-
-    for r in 0..cols {
-        for c in 0..cols {
-            let index = c * cols + r;
-
-            if index < text.len() {
-                encrypted.push(text[index]);
-            }
-        }
-
-        if r < cols - 1 {
-            encrypted.push(' ');
-        }
-    }
-
-    encrypted
+    (0..size)
+        .map(|r| {
+            (0..size)
+                .filter_map(|c| text.get(c * size + r))
+                .collect::<String>()
+        })
+        .collect::<Vec<_>>()
+        .join(" ")
 }
