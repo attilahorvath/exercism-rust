1fbb481a6b0b9bafc23952c151a4b845a6a6de6c
Attila Horváth <hun.ati500@gmail.com>
2018-06-30 04:37:42 +0100

    Improve solution for Simple Linked List

--- simple-linked-list/src/lib.rs
+++ simple-linked-list/src/lib.rs
@@ -1,3 +1,5 @@
+use std::iter::FromIterator;
+
 struct Node<T> {
     data: T,
     next: Option<Box<Node<T>>>,
@@ -9,17 +11,34 @@ impl<T> Node<T> {
     }
 }
 
-struct LinkedListIterator<'a, T: 'a> {
+pub struct LinkedListIter<'a, T: 'a> {
     node: &'a Option<Box<Node<T>>>,
 }
 
-impl<'a, T> Iterator for LinkedListIterator<'a, T> {
-    type Item = &'a Node<T>;
+impl<'a, T> Iterator for LinkedListIter<'a, T> {
+    type Item = &'a T;
 
-    fn next(&mut self) -> Option<&'a Node<T>> {
+    fn next(&mut self) -> Option<&'a T> {
         if let Some(node) = self.node {
             self.node = &node.next;
-            Some(&node)
+            Some(&node.data)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct LinkedListIntoIter<T> {
+    node: Option<Box<Node<T>>>,
+}
+
+impl<T> Iterator for LinkedListIntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<T> {
+        if let Some(mut node) = self.node.take() {
+            self.node = node.next.take();
+            Some(node.data)
         } else {
             None
         }
@@ -53,51 +72,53 @@ impl<T> SimpleLinkedList<T> {
     }
 
     pub fn peek(&self) -> Option<&T> {
-        if let Some(head) = &self.head {
-            Some(&head.data)
-        } else {
-            None
-        }
+        self.iter().next()
     }
 
-    fn iter(&self) -> LinkedListIterator<T> {
-        LinkedListIterator { node: &self.head }
+    fn iter(&self) -> LinkedListIter<T> {
+        LinkedListIter { node: &self.head }
+    }
+}
+
+impl<T> FromIterator<T> for SimpleLinkedList<T> {
+    fn from_iter<I>(iter: I) -> Self
+    where
+        I: IntoIterator<Item = T>,
+    {
+        let mut list = SimpleLinkedList::new();
+
+        for i in iter {
+            list.push(i);
+        }
+
+        list
+    }
+}
+
+impl<T> IntoIterator for SimpleLinkedList<T> {
+    type Item = T;
+    type IntoIter = LinkedListIntoIter<T>;
+
+    fn into_iter(self) -> LinkedListIntoIter<T> {
+        LinkedListIntoIter { node: self.head }
     }
 }
 
 impl<T: Clone> SimpleLinkedList<T> {
     pub fn rev(&self) -> SimpleLinkedList<T> {
-        let mut list = SimpleLinkedList::new();
-
-        for i in self.iter() {
-            list.push(i.data.clone());
-        }
-
-        list
+        self.iter().cloned().collect()
     }
 }
 
 impl<'a, T: Clone> From<&'a [T]> for SimpleLinkedList<T> {
     fn from(item: &[T]) -> Self {
-        let mut list = SimpleLinkedList::new();
-
-        for i in item {
-            list.push(i.clone());
-        }
-
-        list
+        item.iter().cloned().collect()
     }
 }
 
 impl<T> Into<Vec<T>> for SimpleLinkedList<T> {
     fn into(self) -> Vec<T> {
-        let mut vec = Vec::new();
-        let mut node = self.head;
-
-        while let Some(mut n) = node {
-            node = n.next.take();
-            vec.push(n.data);
-        }
+        let mut vec = self.into_iter().collect::<Vec<_>>();
 
         vec.reverse();
         vec
