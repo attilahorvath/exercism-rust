63b38f9d568c2e32484cf205bcc0ae20b46836c6
Attila Horváth <ahorvath@fusioneer.com>
2018-07-06 14:08:16 +0100

    Improve solution for Diffie Hellman to pass the new tests with big numbers

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -2,14 +2,35 @@ extern crate rand;
 
 use rand::Rng;
 
+fn modular_pow(base: u64, exponent: u64, modulus: u64) -> u64 {
+    if modulus == 1 {
+        return 0;
+    }
+
+    let mut result = 1;
+    let mut base = base % modulus;
+    let mut exponent = exponent;
+
+    while exponent > 0 {
+        if exponent % 2 == 1 {
+            result = (result * base) % modulus;
+        }
+
+        exponent >>= 1;
+        base = base.pow(2) % modulus;
+    }
+
+    result
+}
+
 pub fn private_key(p: u64) -> u64 {
     rand::thread_rng().gen_range(2, p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    g.pow(a as u32) % p
+    modular_pow(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    b_pub.pow(a as u32) % p
+    modular_pow(b_pub, a, p)
 }
