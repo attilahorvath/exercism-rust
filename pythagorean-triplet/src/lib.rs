2ed5191b989c9c469a64335b78c8878353522e31
Attila Horváth <hun.ati500@gmail.com>
2018-11-10 03:37:04 +0000

    Improve solution for Pythagorean Triplet

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -3,7 +3,7 @@ pub fn find() -> Option<u32> {
         for b in 1..(1000 - a) {
             let c: u32 = 1000 - a - b;
 
-            if a.pow(2) + b.pow(2) == c.pow(2) && a + b + c == 1000 {
+            if a.pow(2) + b.pow(2) == c.pow(2) {
                 return Some(a * b * c);
             }
         }
